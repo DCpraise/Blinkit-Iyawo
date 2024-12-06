698e78f0f473b9c69c4d64a7cb9eb31858ac7526
DCpraise <chukwupraise72@gmail.com>
2024-12-06 12:03:45 +0100

    second changes

--- client/src/common/SummaryApi.js
+++ client/src/common/SummaryApi.js
@@ -1,4 +1,4 @@
-export const baseURL =   "http://localhost:8080"  //"https://blinkit-iyawo.vercel.app"
+export const baseURL = "https://blinkit-iyawo.vercel.app"  //"http://localhost:8080"  
 
 const SummaryApi = {
     register : {
